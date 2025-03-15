75c161726a0de21bf8eb2ea42e7efb9dd23c3c96
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-03-15 17:00:33 +0700

    fix: un-stub src

--- src/fix_name.rs
+++ src/fix_name.rs
@@ -1,19 +1,19 @@
+use crate::constants::SRC_DIR_NAME;
+use crate::extensions::camino::utf8_path::Utf8Path;
+use crate::extensions::camino::utf8_path_buf::Utf8PathBuf;
+use crate::functions::parent_candidates::parent_candidates;
 use crate::types::outcome::Outcome;
 use anyhow::Context;
 use fs_err::read_to_string;
 use heck::ToSnakeCase;
 use proc_macro2::Ident;
-use stub_macro::stub;
 use syn_more::{maybe_ident_for_item, parse_main_item_from_path};
-
-use crate::extensions::camino::utf8_path::Utf8Path;
-use crate::extensions::camino::utf8_path_buf::Utf8PathBuf;
-use crate::functions::parent_candidates::parent_candidates;
 
 /// This function simply renames the file and the `mod` declaration in the parent module. It doesn't rename all references.
 // TODO: Ensure that every reference is renamed, too (can only do that with rust-analyzer)
 pub fn fix_name(path: &Utf8Path) -> Outcome {
-    let src = stub!();
+    let root = path.get_src_root()?;
+    let src = root.join(SRC_DIR_NAME);
     let ident = main_ident(path)?;
     let module_name_new = ident.to_string().to_snake_case();
     let file_name = format!("{}.rs", module_name_new);
@@ -22,7 +22,7 @@ pub fn fix_name(path: &Utf8Path) -> Outcome {
         let module_name_old = path
             .file_stem()
             .expect("module path should have a file stem");
-        let parent = parent_candidates(path, src).next();
+        let parent = parent_candidates(path, src.as_path()).next();
         fs_err::rename(path, path_new)?;
         if let Some(parent) = parent {
             rename_declarations(parent.as_path(), module_name_old, &module_name_new)?
