3ca438af071fa3b7335b37aaabd00ecbbc462453
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-03-16 21:02:29 +0700

    fix: impl RenameModule for syn::File

--- src/traits/rename_module/impl_rename_module_for_token_stream.rs
+++ src/traits/rename_module/impl_rename_module_for_token_stream.rs
@@ -1,10 +1,9 @@
 use crate::traits::rename_module::RenameModule;
 use derive_more::Error;
 use fmt_derive::Display;
-use proc_macro2::TokenStream;
 
-impl RenameModule for TokenStream {
-    type Output = Result<Self, TokenStreamRenameModuleError>;
+impl RenameModule for syn::File {
+    type Output = Result<Self, SynFileRenameModuleError>;
 
     fn rename_module(self, _module_name_old: &str, _module_name_new: &str) -> Self::Output {
         todo!()
@@ -14,7 +13,7 @@ impl RenameModule for TokenStream {
 type ModuleName = String;
 
 #[derive(Error, Display, Eq, PartialEq, Hash, Clone, Debug)]
-pub enum TokenStreamRenameModuleError {
+pub enum SynFileRenameModuleError {
     #[display("A module with the specified name already exists: {_0}")]
     #[error(ignore)]
     NameAlreadyExists(ModuleName),
@@ -28,27 +27,30 @@ pub enum TokenStreamRenameModuleError {
 mod tests {
     use super::*;
     use pretty_assertions::assert_eq;
-    use proc_macro2::TokenStream;
-    use quote::quote;
-    use TokenStreamRenameModuleError::*;
+    use quote::ToTokens;
+    use syn::parse_file;
+    use SynFileRenameModuleError::*;
 
-    fn input() -> TokenStream {
-        quote! {
-            mod foo;
-            mod bar;
-            mod foobar;
+    const INPUT: &str = "
+        mod foo;
+        mod bar;
+        mod foobar;
 
-            pub use foo::zed::*;
-            pub use foo::other;
-            pub use bar::something;
-            pub use foobar::*;
+        pub use foo::zed::*;
+        pub use foo::other;
+        pub use bar::something;
+        pub use foobar::*;
 
-            pub struct Foo {}
-        }
-    }
+        pub struct Foo {}
+    ";
 
     const FOO: &str = "foo";
+
     const BAR: &str = "bar";
+
+    fn input() -> syn::File {
+        parse_file(INPUT).unwrap()
+    }
 
     /// It must rename the module if the name matches exactly
     /// It must not rename the module if the name starts with, but doesn't match exactly
@@ -56,12 +58,11 @@ mod tests {
     #[ignore]
     fn must_rename_module() {
         let input = input();
-        let input_string = input.to_string();
         let output = input
             .rename_module("foo", "qux")
             .expect("should rename successfully");
-        let output_string_actual = output.to_string();
-        let output_string_expected = input_string
+        let output_string_actual = output.to_token_stream().to_string();
+        let output_string_expected = INPUT
             .replace("mod foo;", "mod qux;")
             .replace("pub use foo::zed::*;", "pub use qux::zed::*;")
             .replace("pub use foo::other;", "pub use qux::other;");
